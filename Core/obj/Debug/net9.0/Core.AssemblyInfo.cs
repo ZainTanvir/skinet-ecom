e837fee0baa4903ed29ffa818644c1544db39b37
Zain <zain@Zains-MacBook-Pro.local>
2025-07-31 00:19:30 +0500

    products controller added and tested - will now move to the architecture

--- Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5a0ab0cd517cdd9c3ba74d69fd519cb9c644ea9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87424c5e7a53d9f05e28d7020349ff00998f714c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
